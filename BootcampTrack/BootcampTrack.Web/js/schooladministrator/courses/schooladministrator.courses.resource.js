c5787447b7115f52395ecb6c531c38190058a4ec
Taylor Smith <taylorsmith4546@gmail.com>
2016-03-17 11:19:40 -0700

    Courses/Branches

---
+++ BootcampTrack/BootcampTrack.Web/js/schooladministrator/courses/schooladministrator.courses.resource.js
@@ -0,0 +1,8 @@
+﻿angular.module('app').factory('CourseResource', function (apiUrl, $resource) {
+    return $resource(apiUrl + '/courses/:courseId', { courseId: '@CourseId' },
+    {
+        'update': {
+            method: 'PUT'
+        }
+    });
+});
